088851160e9b153b0a1abe3625668bea836d1268
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-22 18:13:54 +0530

    Update reg.php

--- lib/views/reg.php
+++ lib/views/reg.php
@@ -10,9 +10,9 @@
         <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="post">
             <label for="Username">Username : </label><br>
             <input type="text" name="username" id="username" class="login-input" required><br>
-            <label for="Username">Password : </label><br>
-            <input type="password" name="password" id="password" class="login-input" required>
-            <label for="Username">Username : </label><br>
+            <label for="Email">Email : </label><br>
+            <input type="email" name="email" id="email" class="login-input" required>
+            <label for="Password">Username : </label><br>
             <input type="text" name="username" id="username" class="login-input" required><br>
             <label for="Username">Password : </label><br>
             <input type="password" name="password" id="password" class="login-input" required>
