2c127fc8298d2a555928e56d277cffad6650508c
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-22 21:33:55 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ var pwdcheck = function() {
         document.getElementById('pwdmsg').innerHTML = '<span><i class="fas fa-check-circle"></i> Password is Match...!</span>';
     } else {
         document.getElementById('pwdmsg').style.color = '#ff3f34';
-        document.getElementById('pwdmsg').innerHTML = '<span><i class="fas fa-check-circle"></i> Password not Match...!</span>';
+        document.getElementById('pwdmsg').innerHTML = '<span><i class="fas fa-window-close"></i> Password not Match...!</span>';
     }
 }
