c705233c9e6303d3f212bd7b557f754314976dfa
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-22 15:25:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php include("lib/layouts/header.php"); ?>
 <?php include("lib/layouts/nav.php"); ?>
 
+<div class="login-content">
+    HI all
+</div>
+
 
 <?php include("lib/layouts/footer.php"); ?>
