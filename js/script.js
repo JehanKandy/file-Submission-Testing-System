7e2c9892d8fa0d94b4b3283978102e4e97de6732
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-22 21:33:00 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,5 +2,8 @@ var pwdcheck = function() {
     if (document.getElementById('password').value == document.getElementById('cpassword').value) {
         document.getElementById('pwdmsg').style.color = '#0f0';
         document.getElementById('pwdmsg').innerHTML = '<span><i class="fas fa-check-circle"></i> Password is Match...!</span>';
+    } else {
+        document.getElementById('pwdmsg').style.color = '#ff3f34';
+        document.getElementById('pwdmsg').innerHTML = '<span><i class="fas fa-check-circle"></i> Password not Match...!</span>';
     }
 }
