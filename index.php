089e0ecb3e25a4c456631c702e7b180abb65bcb2
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-22 17:24:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
     <div class="login-body">
         <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="post">
             <label for="Username">Username : </label><br>
-            <input type="text" name="username" id="username" class="login-input"><br>
+            <input type="text" name="username" id="username" class="login-input" required><br>
             <label for="Username">Password : </label><br>
-            <input type="password" name="password" id="password" class="login-input">
+            <input type="password" name="password" id="password" class="login-input" required>
             <input type="submit" value="Login" name="login" class="login-btn">
         </form>
     </div>
