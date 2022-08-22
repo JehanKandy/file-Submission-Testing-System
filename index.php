e10c8b9f66091b6e37c61bae28f9bca0a7f691eb
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-22 17:14:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,70 +1,20 @@
-@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Fjalla+One&family=Kdam+Thmor+Pro&family=Roboto+Flex:opsz@8..144&family=Rubik&family=Teko:wght@300&display=swap');
+<?php include("lib/layouts/header.php"); ?>
+<?php include("lib/layouts/nav.php"); ?>
 
-/*
+<div class="login-content">
+    <div class="login-title">
+        <i class="fas fa-user-alt"></i> Login Here
+    </div>
+    <div class="login-body">
+        <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="post">
+            <label for="Username">Username : </label><br>
+            <input type="text" name="username" id="username" class="login-input"><br>
+            <label for="Username">Password : </label><br>
+            <input type="password" name="password" id="password" class="login-input">
+            <input type="submit" value="Login" name="login" class="login-btn">
+        </form>
+    </div>
+</div>
 
-use font families
 
-font-family: 'Bebas Neue', cursive;
-font-family: 'Fjalla One', sans-serif;
-font-family: 'Kdam Thmor Pro', sans-serif;
-font-family: 'Roboto Flex', sans-serif;
-font-family: 'Rubik', sans-serif;
-font-family: 'Teko', sans-serif;
-*/
-
-.footer {
-    width: 100%;
-    height: 100%;
-    background-color: rgb(245, 245, 245);
-    border-bottom-left-radius: 5px;
-    border-bottom-right-radius: 5px;
-}
-
-.footer hr {
-    border-top: 3px solid rgb(224, 224, 224);
-}
-
-.footer-body {
-    padding-bottom: 5px;
-    text-align: center;
-    font-family: 'Roboto Flex', sans-serif;
-}
-
-.login-content {
-    width: 590px;
-    height: 400px;
-    background: none;
-    background-position: center;
-    margin-left: 550px;
-    margin-right: 550px;
-    margin-top: 75px;
-    margin-bottom: 50px;
-    border-radius: 10px;
-    border: 1px solid rgb(185, 185, 185);
-}
-
-.login-title {
-    text-align: center;
-    padding-top: 20px;
-    font-family: 'Rubik', sans-serif;
-    font-size: 20px;
-}
-
-.login-body {
-    padding-top: 30px;
-    padding-left: 70px;
-    padding-right: 70px;
-    font-size: 18px;
-}
-
-.login-input {
-    width: 100%;
-    height: 50px;
-    border-radius: 5px;
-    border: 1px solid rgb(192, 192, 192);
-}
-
-.login-btn {
-    margin-top: 15px;
-    width: 100%;
-}
+<?php include("lib/layouts/footer.php"); ?>
