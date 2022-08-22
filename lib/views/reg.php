2f655d73d2f9f89674978fd02205df23a4f515e0
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-22 17:59:04 +0530

    Add files via upload

---
+++ lib/views/reg.php
@@ -0,0 +1,24 @@
+<?php include("../layouts/header.php"); ?>
+<?php include("../layouts/nav.php"); ?>
+<link rel="stylesheet" href="../../css/style.css">
+
+<div class="login-content">
+    <div class="login-title">
+        <i class="fas fa-user-alt"></i> Login Here
+    </div>
+    <div class="login-body">
+        <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="post">
+            <label for="Username">Username : </label><br>
+            <input type="text" name="username" id="username" class="login-input" required><br>
+            <label for="Username">Password : </label><br>
+            <input type="password" name="password" id="password" class="login-input" required>
+            <input type="submit" value="Login" name="login" class="login-btn">
+        </form>
+        <a href="lib/views/reset_pass.php">Forget Password ? </a><br>
+        <hr>
+        Don't have an Account ? <a href="lib/views/reg.php">Create One</a>
+    </div>
+</div>
+
+
+<?php include("../layouts/footer.php"); ?>
