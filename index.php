a30727c7b52a4110d6740dd0b760c865b8fde649
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-22 21:27:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,29 @@
-<?php include("lib/layouts/header.php"); ?>
-<?php include("lib/layouts/nav.php"); ?>
+<?php include("../layouts/header.php"); ?>
+<?php include("../layouts/nav.php"); ?>
+<link rel="stylesheet" href="../../css/style.css">
 
-<div class="login-content">
+<div class="reg-content">
     <div class="login-title">
-        <i class="fas fa-user-alt"></i> Login Here
+        <i class="fas fa-user-plus"></i> Register Here
     </div>
     <div class="login-body">
         <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="post">
             <label for="Username">Username : </label><br>
             <input type="text" name="username" id="username" class="login-input" required><br>
+            <label for="Email">Email : </label><br>
+            <input type="email" name="email" id="email" class="login-input" required><br>
             <label for="Password">Password : </label><br>
-            <input type="password" name="password" id="password" class="login-input" required>
-            <input type="submit" value="Login" name="login" class="login-btn">
+            <input type="password" name="password" id="password" class="login-input" onkeyup="pwdcheck()" required><br>
+            <label for="Cpassword">Confirm Password : </label><br>
+            <input type="password" name="cpassword" id="cpassword" class="login-input" onkeyup="pwdcheck()" required>
+            <p id="pwdmsg"></p>
+            <input type="reset" value="Clear" class="clear-btn">
+            <input type="submit" value="Register" name="register" class="reg-btn">
         </form>
-        <a href="lib/views/reset_pass.php">Forget Password ? </a><br>
         <hr>
-        Don't have an Account ? <a href="lib/views/reg.php">Create One</a>
+        Already have an Account ? <a href="../../index.php">Login</a>
     </div>
 </div>
 
 
-<?php include("lib/layouts/footer.php"); ?>
+<?php include("../layouts/footer.php"); ?>
