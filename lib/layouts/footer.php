03b3c4aeeb71657cbe1927cede0d642db68ca1c0
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-22 15:14:35 +0530

    Update footer.php

--- lib/layouts/footer.php
+++ lib/layouts/footer.php
@@ -1,3 +1,6 @@
 <div class="footer">
     <hr>
+    <div class="footer-body">
+        Copyright <i class="far fa-copyright"></i> file Submission Testing System : 2022 All Right Reserved by : <a href="https://github.com/JehanKandy">JehanKandy</a>
+    </div>
 </div>
