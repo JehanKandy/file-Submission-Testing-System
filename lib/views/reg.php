d6480297891a05c5e5ef48444924fc1d1ca321da
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-23 08:43:35 +0530

    Update reg.php

--- lib/views/reg.php
+++ lib/views/reg.php
@@ -2,6 +2,7 @@
 <?php include("../layouts/nav.php"); ?>
 <link rel="stylesheet" href="../../css/style.css">
 
+
 <div class="reg-content">
     <div class="login-title">
         <i class="fas fa-user-plus"></i> Register Here
@@ -9,13 +10,14 @@
     <div class="login-body">
         <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="post">
             <label for="Username">Username : </label><br>
-            <input type="text" name="username" id="username" class="login-input" required><br>
+            <input type="text" name="username" id="username" class="login-input" onkeyup="pwdcheck()" required><span id="uname"></span><br>
             <label for="Email">Email : </label><br>
-            <input type="email" name="email" id="email" class="login-input" required><br>
+            <input type="email" name="email" id="email" class="login-input" onkeyup="pwdcheck()" required><span id="uemail"></span><br>
             <label for="Password">Password : </label><br>
-            <input type="password" name="password" id="password" class="login-input" onkeyup="pwdcheck()" required><br>
+            <input type="password" name="password" id="password" class="login-input" onkeyup="pwdcheck()" required><span id="upwd"></span><br>
             <label for="Cpassword">Confirm Password : </label><br>
-            <input type="password" name="cpassword" id="cpassword" class="login-input" onkeyup="pwdcheck()" required>
+            <input type="password" name="cpassword" id="cpassword" class="login-input" onkeyup="pwdcheck()" required><span id="ucpwd"></span>
+            <p id="pwdmsg"></p>
             <input type="reset" value="Clear" class="clear-btn">
             <input type="submit" value="Register" name="register" class="reg-btn">
         </form>
@@ -24,5 +26,6 @@
     </div>
 </div>
 
+<script src="../../js/script.js"></script>
 
 <?php include("../layouts/footer.php"); ?>
