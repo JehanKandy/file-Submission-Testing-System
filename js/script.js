6d19aceed44fd5ccb309ae2dfeb60090dc030865
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-23 10:10:05 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+function validateform() {
+
+}
+
+
 var pwdcheck = function() {
     if (document.getElementById('password').value == document.getElementById('cpassword').value) {
         document.getElementById('pwdmsg').style.color = '#0f0';
