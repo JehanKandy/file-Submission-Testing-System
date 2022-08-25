d16795aa057e9757a9f2ef0a20cd4f52eddbc80a
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-25 20:12:08 +0530

    Update waiting.php

--- lib/views/waiting.php
+++ lib/views/waiting.php
@@ -1,6 +1,11 @@
 <?php include("../layouts/header.php"); ?>
 <?php include("../layouts/nav.php"); ?>
 <link rel="stylesheet" href="../../css/style.css">
-hi all
+
+<div class="waiting-content">
+    <div class="waiting-title">
+        
+    </div>
+</div>
 
 <?php include("../layouts/footer.php"); ?>
