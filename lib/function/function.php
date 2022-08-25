7f6dc22fb770366251e1911d6f88af030f0f3d05
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-25 19:49:00 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -10,12 +10,18 @@
         $check_user_result = mysqli_query($con, $check_user);
         $user_nor = mysqli_num_rows($check_user_result);
         
+        $_SESSION['userId'] = $email;
         if($user_nor > 0){
             return "<span style='color:red;'>User Already Exists..!</span>";
         }else{
             $insert_user = "INSERT INTO user_tbl(username,email,pass1,roll,user_status)VALUES('$username','$email','$password','student','1')";
             $insert_user_result = mysqli_query($con,$insert_user);
+            header("location:../../index.php");
         }
+    }
+
+    function user_id(){
+        
     }
     
 
