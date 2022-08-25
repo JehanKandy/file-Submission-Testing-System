d53defe375082a02006697aa94288ccbd68b83c3
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-25 20:50:42 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -31,7 +31,8 @@
 
         $login_user = "SELECT * FROM user_tbl WHERE username = '$login_username' && '$login_password' && is_pending = '0' && user_status = '1'";
         $login_user_result = mysqli_query($con,$login_user);
-        
+        $login_user_nor = mysqli_num_rows($login_user_result);
+
     }
 
     
