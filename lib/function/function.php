a8de7367e8047688c0cd9095fd51c74d04f8b7a6
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-25 20:45:26 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -14,7 +14,7 @@
         if($user_nor > 0){
             return "<span style='color:red;'>User Already Exists..!</span>";
         }else{
-            $insert_user = "INSERT INTO user_tbl(username,email,pass1,roll,user_status)VALUES('$username','$email','$password','student','1')";
+            $insert_user = "INSERT INTO user_tbl(username,email,pass1,roll,is_pending,user_status)VALUES('$username','$email','$password','student','1','0')";
             $insert_user_result = mysqli_query($con,$insert_user);
             header("location:../views/waiting.php");            
         }
