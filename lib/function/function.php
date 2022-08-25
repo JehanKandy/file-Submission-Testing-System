416ab87fe5435a526d63e61b4de6cfe97a9aee66
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-25 19:15:27 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -8,7 +8,11 @@
 
         $check_user = "SELECT * FROM user_tbl WHERE username = '$username' && email = '$email'";
         $check_user_result = mysqli_query($con, $check_user);
-        $user_nor = mysqli_num_rows($check_user_result);        
+        $user_nor = mysqli_num_rows($check_user_result);
+        
+        if($user_nor > 0){
+            return "<span style='color:red;'>User Already Exists..!</span>";
+        }
     }
     
 
