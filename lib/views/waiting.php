0949448f0ed0a413d74ffbb2c9df727cc12963ca
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-25 20:10:25 +0530

    Update waiting.php

--- lib/views/waiting.php
+++ lib/views/waiting.php
@@ -1,6 +1,6 @@
 <?php include("../layouts/header.php"); ?>
 <?php include("../layouts/nav.php"); ?>
-
+<link rel="stylesheet" href="../../css/style.css">
 hi all
 
 <?php include("../layouts/footer.php"); ?>
