fd9a08f7caf817112a05c768bc361b354ac3d958
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-26 19:31:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <?php include("lib/layouts/header.php"); ?>
 <?php include("lib/layouts/nav.php"); ?>
+<?php include("lib/function/function.php"); ?>
 
 <div class="login-content">
     <div class="login-title">
         <i class="fas fa-user-alt"></i> Login Here
     </div>
     <div class="login-body">
-        <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="post" >
+        <?php 
+            if(isset($_POST['login'])){
+
+            }
+        ?> 
+        <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="post" name="loginform" onsubmit="return validatelogin()">
             <label for="Username">Username : </label><br>
-            <input type="text" name="username" id="username" class="login-input"><span id="usererror"></span>
+            <input type="text" name="loginusername" id="loginusername" class="login-input"><span id="usererror"></span><br>
             <label for="Password">Password : </label><br>
-            <input type="password" name="password" id="password" class="login-input"><span id="userpasserror"></span>
+            <input type="password" name="loginpassword" id="loginpassword" class="login-input"><span id="userpasserror"></span>
             <input type="submit" value="Login" name="login" class="login-btn">
         </form>
         <a href="lib/views/reset_pass.php">Forget Password ? </a><br>
@@ -20,4 +26,5 @@
 </div>
 
 <script src="js/script.js"></script>
+
 <?php include("lib/layouts/footer.php"); ?>
