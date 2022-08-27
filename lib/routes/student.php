51eca27e52dcc635634fc677abdbe4bbd3e02eec
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-27 22:01:11 +0530

    Update student.php

--- lib/routes/student.php
+++ lib/routes/student.php
@@ -1,2 +1,7 @@
 <?php include("../layouts/header.php"); ?>
 <?php include("../layouts/nav_loged.php"); ?>
+<?php
+if (empty($_SESSION['LoginSession'])) {
+    header('location:../../index.php');
+}
+?>
